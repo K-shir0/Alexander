a0e348be06df453d2c5e882e59c2f7d24f73c345
K-shir0 <kakishiro2190394@gmail.com>
2021-07-04 01:35:41 +0900

    add マンダラチャートの雛形を作成

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:alexander/pages/auth_sample_page.dart';
+import 'package:alexander/pages/mandala_chart_page.dart';
 import 'package:alexander/service/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -33,10 +34,11 @@ class App extends HookWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/test',
+      initialRoute: '/mandala',
       routes: {
         '/': (BuildContext context) => CounterPage(title: 'Flutter Demo Home Page'),
         '/test': (BuildContext context) => AuthSamplePage(),
+        '/mandala': (BuildContext context) => MandalaChartPage(),
       },
     );
   }
---
+++ lib/pages/mandala_chart_page.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class MandalaChartPage extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: Column(
+          children: [
+            Row(
+              children: [
+                MandalaBox(),
+                MandalaBox(),
+                MandalaBox(),
+              ],
+            ),
+            Row(
+              children: [
+                MandalaBox(),
+                MandalaBox(),
+                MandalaBox(),
+              ],
+            ),
+            Row(
+              children: [
+                MandalaBox(),
+                MandalaBox(),
+                MandalaBox(),
+              ],
+            ),
+          ],
+        ));
+  }
+}
+
+class MandalaBox extends HookWidget {
+  KeyEventResult _handleKeyPress(FocusNode node, RawKeyEvent event) {
+    if (event is RawKeyDownEvent) {
+      print('test ${node.offset}');
+
+      return KeyEventResult.handled;
+    }
+    return KeyEventResult.ignored;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final offset = useState(const Offset(0, 0));
+    final isFocus = useState(false);
+
+    Widget _card(bool hasFocus) {
+      return SizedBox(
+        height: 128,
+        width: 128,
+        child: Card(
+          child: Column(
+            children: [
+              TextFormField(
+                enabled: hasFocus,
+                initialValue: 'test',
+              ),
+              Text(hasFocus ? 'true' : 'false'),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Focus(
+      onFocusChange: (hasFocus) {
+        if (hasFocus) {
+        } else {
+          isFocus.value = false;
+          print('毎回呼ばれる');
+        }
+      },
+      child: Builder(builder: (BuildContext context) {
+        final FocusNode focusNode = Focus.of(context);
+        final bool hasFocus = focusNode.hasFocus;
+
+        return GestureDetector(
+          onTap: () {
+            if (!hasFocus) {
+              focusNode.requestFocus();
+              print('test');
+              isFocus.value = true;
+            }
+          },
+          onPanUpdate: (_) {
+            offset.value += _.delta;
+
+            print(offset.value);
+          },
+          onPanEnd: (_) {
+            offset.value = const Offset(0, 0);
+          },
+          child: Transform.translate(
+            offset: offset.value,
+            child: _card(isFocus.value),
+          ),
+        );
+      }),
+    );
+  }
+}
