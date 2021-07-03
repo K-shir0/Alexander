f6cf54279c84d1c758b59f089250c0a299a03ba7
K-shir0 <kakishiro2190394@gmail.com>
2021-07-03 14:49:07 +0900

    add APIからのレスポンスを処理するクラスを追加

---
+++ lib/service/base/model/result.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'result.freezed.dart';
+
+// part 'result.g.dart';
+
+@freezed
+abstract class Result<T> with _$Result<T> {
+  const factory Result.success(T value) = Success<T>;
+
+  const factory Result.failure(Error error) = Failure<T>;
+
+// factory Result.fromJson(Map<String, dynamic> json) => _$ResultFromJson(json);
+}
