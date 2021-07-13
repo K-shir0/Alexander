7d87bfc28528f1c2c6849a938cb04d9b3b5d3f75
Kyoichi <kyoichi.807@gmail.com>
2021-07-13 12:16:02 +0900

    検索フィールドの完成

--- lib/pages/ideaserch_page.dart
+++ lib/pages/ideaserch_page.dart
@@ -8,41 +8,41 @@ class IdeaPage extends StatefulWidget {
 }
 
 class _IdeaPageState extends State<IdeaPage> {
-  //検索バーの出現を管理するための変数
-  bool searchcheck = true;
-
   @override
   Widget build(BuildContext context) {
+    //画面サイズを取得
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Alexander IdeaserchPage'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.search),
-            //検索ボタンをクリック時
-            onPressed: () {},
-          ),
-        ],
       ),
       body: Container(
         padding: const EdgeInsets.all(20),
-        child: Row(
+        child: Column(
           children: [
             //true = 表示, false = 非表示
-            if (searchcheck == true)
-              const SizedBox(
-                height: 100,
-                width: 100,
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Text(''),
+                SizedBox(
+                  width: size.width / 4,
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      prefixIcon: Icon(Icons.search),
+                      hintText: '検索',
                     ),
-                    prefixIcon: Icon(Icons.search),
-                    hintText: '検索',
                   ),
                 ),
-              ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('ボツリスト'),
+                )
+              ],
+            ),
           ],
         ),
       ),
