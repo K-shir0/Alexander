f596e9641a956ebb2570e5c9e293d6c13d28206d
HirohikoFujio <hirohiko0104@gmail.com>
2021-07-15 11:58:28 +0900

    カラーパレット

---
+++ lib/pages/theme/palette.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class Palette{
+  static const Color signButtonColor = Color(0xffffc400);
+  static const Color mainTextColor = Color(0xff83acb4);
+  static const Color accentTextColor = Color(0xff687c80);
+  static const Color titleTextColor = Color(0xff323b50);
+  static const Color emphasisTetxColor = Color(0xffff6c00);
+
+  static const Color bgContentsColor = Color(0xffe9eff0);
+  static const Color bgContentsHighLightColor = Color(0xfff0f5f5);
+}
