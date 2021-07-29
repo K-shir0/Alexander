163da5d6e1b0163ed398c1b5599ce2f6f4c787f7
K-shir0 <kakishiro2190394@gmail.com>
2021-07-29 10:27:35 +0900

    fix #35

--- lib/pages/common/alexander_text_field.dart
+++ lib/pages/common/alexander_text_field.dart
@@ -6,12 +6,19 @@ class AlexanderTextField extends StatelessWidget {
   final IconData iconName;
   final String label;
   final String engLabel;
-  final String infomation;
+  final String? information;
   final bool obscureText;
+  final TextEditingController? controller;
 
-
-  const AlexanderTextField({Key? key, required this.iconName,required this.label, required this.engLabel, required this.infomation,  this.obscureText = false}) : super(key: key);
-
+  const AlexanderTextField({
+    Key? key,
+    required this.iconName,
+    required this.label,
+    required this.engLabel,
+    this.information,
+    this.obscureText = false,
+    this.controller,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,67 +27,74 @@ class AlexanderTextField extends StatelessWidget {
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
+            //左上テキストラベル
             Row(
               children: <Widget>[
                 Padding(
                   padding: const EdgeInsets.only(bottom: 8.0, right: 8.0),
-                  child: Icon(iconName,
-                      color: Palette.mainTextColor),
+                  child: Icon(iconName, color: Palette.mainTextColor),
                 ),
                 // Column(
                 //   children: [
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 8.0),
-                      child: Text(
-                        label,
-                        style: const TextStyle(
-                            fontSize: 12,
-                            color: Palette.mainTextColor,
-                            letterSpacing: 2.4000000000000004,),
-                      ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    label,
+                    style: const TextStyle(
+                      fontSize: 12,
+                      color: Palette.mainTextColor,
+                      letterSpacing: 2.4000000000000004,
                     ),
+                  ),
+                ),
                 //   ],
                 // ),
               ],
             ),
-             Padding(
-               padding: const EdgeInsets.only(bottom: 8.0),
-               child: Text(engLabel,
+            //右上テキストラベル
+            Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: Text(engLabel,
                   style: const TextStyle(
                     fontSize: 12,
                     color: Palette.accentTextColor,
                   )),
-             )
+            )
           ],
         ),
+        //入力テキストフィールド
         Container(
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(4.0),
             color: const Color(0xffedf0f0),
           ),
-          child: TextField(
+          child: TextFormField(
+            controller: controller,
             obscureText: obscureText,
             decoration: const InputDecoration(
               border: InputBorder.none,
+              contentPadding: EdgeInsets.all(20.0),
               // labelText: engLabel,
             ),
           ),
         ),
+        //error用テキストラベル
         Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
             Row(
               children: <Widget>[
-                 const Padding(
-                  padding: EdgeInsets.only(top: 8.0, right: 8.0),
-                  child: Icon(Icons.info_outline_rounded,
-                      color: Palette.emphasisTextColor),
-                ),
+                if (information != null)
+                  const Padding(
+                    padding: EdgeInsets.only(top: 8.0, right: 8.0),
+                    child: Icon(Icons.info_outline_rounded,
+                        color: Palette.emphasisTextColor),
+                  ),
                 Padding(
                   padding: const EdgeInsets.only(top: 8.0),
                   child: Text(
-                    infomation,
-                    style:  const TextStyle(
+                    information ?? '',
+                    style: const TextStyle(
                       fontSize: 12,
                       color: Palette.emphasisTextColor,
                       letterSpacing: 2.4000000000000004,
